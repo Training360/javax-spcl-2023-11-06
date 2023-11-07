f30df0536546a385fd4c0f8429dd9a10baa2b00c
Viczián István <viczian.istvan@gmail.com>
2023-11-07 13:04:27 +0100

    GraphQL befejezés

--- bff-service/src/main/java/mentoring/bffservice/controller/CoursesController.java
+++ bff-service/src/main/java/mentoring/bffservice/controller/CoursesController.java
@@ -1,15 +1,19 @@
 package mentoring.bffservice.controller;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import mentoring.bffservice.coursegateway.CourseResource;
 import mentoring.bffservice.service.CoursesService;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.graphql.data.method.annotation.SchemaMapping;
 import org.springframework.stereotype.Controller;
+import mentoring.bffservice.service.CompositeCourseResource;
 
 import java.util.List;
 
 @Controller
 @AllArgsConstructor
+@Slf4j
 public class CoursesController {
 
     private CoursesService coursesService;
@@ -18,4 +22,12 @@ public class CoursesController {
     public List<CourseResource> courses() {
         return coursesService.findAllCourses();
     }
+
+    @SchemaMapping(typeName = "Course", field = "courseDetails")
+    public CompositeCourseResource courseDetails(CourseResource resource) {
+        log.info("Call courseDetails: {}", resource);
+        return coursesService.findCourseById(resource.getId());
+    }
+
+    // http://localhost:8085/api/courses/details?id=1,5,8
 }
